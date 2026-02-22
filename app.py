c45aec4fc7a3d341a51a3d633fc791f09c3a38c6
Vaibhav Kumar <vaibhavkumar@Vaibhavs-MacBook-Air-2.local>
2026-02-22 12:22:05 +0530

    Added sales data and analysis script

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
-print("Hello, GitHub!")
-print("I updated my code!")
+import pandas as pd
+
+# Load CSV
+df = pd.read_csv("simple_sales_data.csv")
+
+# Show first 5 rows
+print("First 5 rows:")
+print(df.head())
+
+# Total revenue
+print("\nTotal Revenue:", df["Total_Revenue"].sum())
+
+# Revenue by region
+print("\nRevenue by Region:")
+print(df.groupby("Region")["Total_Revenue"].sum())
